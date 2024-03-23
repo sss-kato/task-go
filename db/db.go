5aa2450409a482e17824eccdf26375d30340df4f
sss-kato <syunpei.1124@gmail.com>
2024-03-23 16:06:28 +0900

    DB接続 開発環境用の処理削除

--- db/db.go
+++ db/db.go
@@ -5,19 +5,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
-
-	if os.Getenv("GO_ENV") == "dev" {
-		err := godotenv.Load(".env")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 		os.Getenv("CURRENT_DB_USER"),
