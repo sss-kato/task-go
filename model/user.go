80f40e874c87f4945405b56ee73de0a7deea1c3d
sss-kato <syunpei.1124@gmail.com>
2024-04-03 22:18:44 +0900

    User構造体を分離

--- model/user.go
+++ model/user.go
@@ -10,20 +10,3 @@ type User struct {
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
-
-type UserResponse struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
-}
-
-// type User struct {
-// 	ID        uint   `gorm:"primaryKey"`
-// 	Name      string `gorm:"unique;not null"`
-// 	Password  string
-// 	CreatedAt time.Time
-// 	UpdatedAt time.Time
-// }
-// type UserResponse struct {
-// 	ID   uint   `json:"id" gorm:"primaryKey"`
-// 	Name string `json:"name" gorm:"unique;not null"`
-// }
---
+++ model/user_response.go
@@ -0,0 +1,6 @@
+package model
+
+type UserResponse struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
