ba36a0b473679e031258c83bf2fa3b1b4d9528be
sss-kato <syunpei.1124@gmail.com>
2024-05-08 19:13:50 +0900

    不要なコード削除

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -33,7 +33,6 @@ func (uc *userController) Signup(c echo.Context) error {
 	userReq := &UserRequest{}
 
 	if bindErr := c.Bind(userReq); bindErr != nil {
-		// return err
 		util.WriteErrLog(bindErr)
 		return c.JSON(http.StatusBadRequest, bindErr.Error())
 	}
@@ -44,8 +43,6 @@ func (uc *userController) Signup(c echo.Context) error {
 	if nameErr != nil {
 		errMsg := &domain.Message{Message: nameErr.Error()}
 
-		// errorLog := util.SetErrorInfo(nameErr)
-		// util.WriteErrLog(errorLog)
 		util.WriteErrLog(nameErr)
 
 		return c.JSON(http.StatusBadRequest, errMsg)
@@ -55,7 +52,6 @@ func (uc *userController) Signup(c echo.Context) error {
 	if pwErr != nil {
 		errMsg := &domain.Message{Message: pwErr.Error()}
 
-		// errorLog := util.SetErrorInfo(pwErr)
 		util.WriteErrLog(pwErr)
 
 		return c.JSON(http.StatusBadRequest, errMsg)
@@ -65,7 +61,6 @@ func (uc *userController) Signup(c echo.Context) error {
 	if mailErr != nil {
 		errMsg := &domain.Message{Message: mailErr.Error()}
 
-		// errorLog := util.SetErrorInfo(mailErr)
 		util.WriteErrLog(mailErr)
 
 		return c.JSON(http.StatusBadRequest, errMsg)
@@ -75,8 +70,6 @@ func (uc *userController) Signup(c echo.Context) error {
 	if signupErr != nil {
 		errMsg := &domain.Message{Message: "signup failed."}
 
-		// errorLog := util.SetErrorInfo(signupErr)
-		// util.WriteErrLog(errorLog)
 		util.WriteErrLog(signupErr)
 
 		return c.JSON(http.StatusInternalServerError, errMsg)
@@ -85,43 +78,3 @@ func (uc *userController) Signup(c echo.Context) error {
 	return c.JSON(http.StatusCreated, userRes)
 
 }
-
-// func test(reqStruct interface{}) interface{} {
-// 	reqStructType := reflect.TypeOf(reqStruct)
-// 	// reqStructValue := reflect.ValueOf(&reqStruct).Elem()
-// 	reqStructValue := reflect.ValueOf(reqStruct)
-// 	fmt.Print(reqStructValue.Kind())
-
-// 	for i := 0; i < reqStructType.NumField(); i++ {
-// 		field := reqStructValue.Field(i)
-// 		// サニタイズが必要かチェック
-// 		if reqStructType.Field(i).Tag.Get("sanitized") == "true" {
-// 			sanitizedValue := sanitizeField(field)
-// 			test := "test"
-// 			ptest := &test
-
-// 			fmt.Print(sanitizedValue)
-// 			// サニタイズされた値をセット
-// 			// field.Set(reflect.ValueOf(sanitizedValue))
-// 			a := reflect.ValueOf(ptest)
-// 			pa := &a
-// 			// field.Set(reflect.ValueOf(ptest))
-// 			field.Set(a)
-
-// 		}
-// 	}
-// 	fmt.Print(reqStruct)
-
-// 	return nil
-// }
-
-// func sanitizeField(field reflect.Value) interface{} {
-// 	switch field.Kind() {
-// 	case reflect.String:
-// 		// string型の場合はHTMLエスケープを実行
-// 		return html.EscapeString(field.String())
-// 	default:
-// 		// その他の型の場合はそのまま返す
-// 		return field.Interface()
-// 	}
-// }
