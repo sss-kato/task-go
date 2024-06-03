62ed9c7b07b75d8668d43b67eb5ad1dbc4187d6c
sss-kato <syunpei.1124@gmail.com>
2024-06-03 20:32:22 +0900

    ログイン コントローラークラス テストコード

--- controller/user_controller_test.go
+++ controller/user_controller_test.go
@@ -9,6 +9,7 @@ import (
 	"task-go/domain"
 	"task-go/service"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
@@ -132,6 +133,79 @@ func Test_userController_Signup(t *testing.T) {
 			if expectedJSON != "" {
 
 				assert.JSONEq(t, expectedJSON, rec.Body.String(), fmt.Sprintf("expected JSON: %s; actual JSON: %s", expectedJSON, rec.Body.String()))
+			}
+
+		})
+	}
+}
+
+func Test_userController_Login(t *testing.T) {
+
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	mock := service.NewMockUserServiceIF(mockCtl)
+
+	// test case1
+	user1, _ := domain.NewUser("test1", "test1", "test1@gmail.com")
+	mock.EXPECT().Login(user1).Return("test", nil)
+	e := echo.New()
+	req1 := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(`{"name":"test1","password":"test1","mailadress":"test1@gmail.com"}`))
+	req1.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec1 := httptest.NewRecorder()
+	c := e.NewContext(req1, rec1)
+
+	recMap := map[string]*httptest.ResponseRecorder{"case1": rec1}
+
+	type fields struct {
+		us service.UserServiceIF
+	}
+	type args struct {
+		c echo.Context
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"case1", fields{mock}, args{c}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			uc := &userController{
+				us: tt.fields.us,
+			}
+			if err := uc.Login(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("userController.Login() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			rec := recMap[tt.name]
+			expectedHttpStatusCode := 0
+			switch tt.name {
+			case "case1":
+				expectedHttpStatusCode = http.StatusOK
+			case "case2":
+				expectedHttpStatusCode = http.StatusInternalServerError
+
+			case "case3", "case4", "case5", "case6":
+				expectedHttpStatusCode = http.StatusBadRequest
+			}
+
+			if expectedHttpStatusCode != 0 && rec.Code != expectedHttpStatusCode {
+				t.Errorf("expected status %d; got %d", expectedHttpStatusCode, rec.Code)
+
+			}
+			if tt.name == "case1" && rec.Result().Cookies()[0].Name != "token" {
+				t.Errorf("expected cookie name %s; got %s", "token", rec.Result().Cookies()[0].Name)
+			}
+
+			if tt.name == "case1" && rec.Result().Cookies()[0].Value != "test" {
+				t.Errorf("expected cookie name %s; got %s", "test", rec.Result().Cookies()[0].Value)
+			}
+
+			if tt.name == "case1" && rec.Result().Cookies()[0].Expires.After(time.Now().Add(24*time.Hour)) {
+				t.Error("cookie time is wrong")
 			}
 
 		})
