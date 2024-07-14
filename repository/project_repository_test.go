29eb734a8d98fc162eb8bd39bd7c02170255b926
sss-kato <syunpei.1124@gmail.com>
2024-07-14 18:41:32 +0900

    sql-mock導入

--- repository/project_repository_test.go
+++ repository/project_repository_test.go
@@ -1,40 +1,42 @@
 package repository
 
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 // func Test_projectRepository_CreateProject(t *testing.T) {
-// 	jst, _ := time.LoadLocation("Asia/Tokyo")
-// 	now := time.Now().In(jst)
-// 	// formattedNow := now.Format("2006-01-02 15:04:05.000")
+
+// 	gormDB := createDbMockCon(t)
+
+// 	// test case1
 // 	test1ProjectDto := &dto.ProjectDto{Name: "testproject1", UserID: 1}
-// 	test2ProjectDto := &dto.ProjectDto{Name: "testproject2", UserID: 2, CreatedAt: now, UpdatedAt: now}
-
-// 	dbm, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-// 	}
-// 	defer dbm.Close()
-// 	dialector := postgres.New(postgres.Config{
-// 		DSN:                  "sqlmock_db_0",
-// 		DriverName:           "postgres",
-// 		Conn:                 dbm,
-// 		PreferSimpleProtocol: true,
-// 	})
-// 	gormDB, err := gorm.Open(dialector, &gorm.Config{})
-// 	if err != nil {
-// 		t.Fatalf("an error '%s' was not expected when initializing a gorm db connection", err)
-// 	}
-
-// 	mock.ExpectBegin()
-
-// 	rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
-// 	mock.ExpectQuery(regexp.QuoteMeta(
+// 	mock1 := createDbMock(t)
+// 	mock1.ExpectBegin()
+// 	rows1 := sqlmock.NewRows([]string{"id"}).AddRow(1)
+// 	mock1.ExpectQuery(regexp.QuoteMeta(
 // 		`INSERT INTO "projects" ("name","user_id","created_at","updated_at") VALUES ($1,$2,$3,$4)`)).
-// 		WillReturnRows(rows)
-// 	mock.ExpectCommit()
+// 		WillReturnRows(rows1)
 
-// 	mock.ExpectExec("INSERT INTO \"project_user_mappings\"").
-// 		WithArgs(sqlmock.AnyArg(), test2ProjectDto.UserID).
+// 	// test case 2
+// 	test2ProjectDto := &dto.ProjectDto{Name: "testproject2", UserID: 2}
+
+// 	mock2 := createDbMock(t)
+// 	mock2.ExpectBegin()
+
+// 	rows2 := sqlmock.NewRows([]string{"id"}).AddRow(5)
+// 	mock2.ExpectQuery(regexp.QuoteMeta(
+// 		`INSERT INTO "projects" ("name","user_id","created_at","updated_at") VALUES ($1,$2,$3,$4)`)).
+// 		WillReturnRows(rows2)
+
+// 	mock2.ExpectExec(regexp.QuoteMeta(`INSERT INTO "project_user_mappings" ("project_id","user_id") VALUES ($1,$2)`)).
+// 		WithArgs(5, test2ProjectDto.UserID).
 // 		WillReturnError(errors.New("insertion error"))
-// 	mock.ExpectRollback()
+
+// 	mock2.ExpectRollback()
 
 // 	type fields struct {
 // 		db *gorm.DB
@@ -62,10 +64,42 @@ package repository
 // 			}
 
 // 			if tt.name == "case2" {
-// 				if err := mock.ExpectationsWereMet(); err != nil {
+// 				if err := mock2.ExpectationsWereMet(); err != nil {
 // 					t.Errorf("there were unfulfilled expectations: %s", err)
 // 				}
 // 			}
 // 		})
 // 	}
 // }
+
+func createDbMockCon(t *testing.T) *gorm.DB {
+
+	db, _, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	dialector := postgres.New(postgres.Config{
+		DSN:                  "sqlmock_db_0",
+		DriverName:           "postgres",
+		Conn:                 db,
+		PreferSimpleProtocol: true,
+	})
+	gormDB, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when initializing a gorm db connection", err)
+	}
+
+	return gormDB
+}
+
+func createDbMock(t *testing.T) sqlmock.Sqlmock {
+
+	_, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return mock
+
+}
