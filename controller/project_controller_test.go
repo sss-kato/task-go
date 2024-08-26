74b97428f581dfe85b7cc6307c75da611d35c457
sss-kato <syunpei.1124@gmail.com>
2024-08-26 21:06:39 +0900

    プロジェクトコントローラークラス テストコード作成

--- controller/project_controller_test.go
+++ controller/project_controller_test.go
@@ -1,16 +1,30 @@
 package controller
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"task-go/domain"
 	"task-go/service"
 	"testing"
 
+	gomock "github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
 )
 
 func Test_projectController_CreateProject(t *testing.T) {
-	// mockCtl := gomock.NewController(t)
-	// defer mockCtl.Finish()
-	// mock := service.NewMockProjectServiceIF(mockCtl)
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+	mock := service.NewMockProjectServiceIF(mockCtl)
+
+	test_project1, _ := domain.NewProject("test1", 1)
+	mock.EXPECT().CreateProject(test_project1).Return(nil)
+
+	e1 := echo.New()
+	req1 := httptest.NewRequest(http.MethodPost, "/project/create", strings.NewReader(`{"name":"test1","userid":1}`))
+	req1.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec1 := httptest.NewRecorder()
+	c1 := e1.NewContext(req1, rec1)
 
 	type fields struct {
 		ps service.ProjectServiceIF
@@ -25,6 +39,7 @@ func Test_projectController_CreateProject(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{"case1", fields{mock}, args{c1}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
