398005cc10c048b6fcc8e12bb475a3765e22a1d2
sss-kato <syunpei.1124@gmail.com>
2024-09-02 08:06:03 +0900

    プロジェクトクラス　テストコード作成

--- controller/project_controller.go
+++ controller/project_controller.go
@@ -44,7 +44,7 @@ func (pc *projectController) CreateProject(c echo.Context) error {
 
 	createPjErr := pc.ps.CreateProject(project)
 	if createPjErr != nil {
-		errMsg := &domain.Message{Message: domain.ErrorMsg08}
+		errMsg := &domain.Message{Message: domain.ErrorMsg13}
 		util.WriteErrLog(createPjErr)
 		return c.JSON(http.StatusInternalServerError, errMsg)
 	}
--- controller/project_controller_test.go
+++ controller/project_controller_test.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -10,21 +12,34 @@ import (
 
 	gomock "github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_projectController_CreateProject(t *testing.T) {
 	mockCtl := gomock.NewController(t)
 	defer mockCtl.Finish()
-	mock := service.NewMockProjectServiceIF(mockCtl)
 
+	mock1 := service.NewMockProjectServiceIF(mockCtl)
 	test_project1, _ := domain.NewProject("test1", 1)
-	mock.EXPECT().CreateProject(test_project1).Return(nil)
+	mock1.EXPECT().CreateProject(test_project1).Return(nil)
 
 	e1 := echo.New()
 	req1 := httptest.NewRequest(http.MethodPost, "/project/create", strings.NewReader(`{"name":"test1","userid":1}`))
 	req1.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec1 := httptest.NewRecorder()
 	c1 := e1.NewContext(req1, rec1)
+
+	mock2 := service.NewMockProjectServiceIF(mockCtl)
+	test_project2, _ := domain.NewProject("test1", 1)
+	mock2.EXPECT().CreateProject(test_project2).Return(errors.New("error"))
+
+	e2 := echo.New()
+	req2 := httptest.NewRequest(http.MethodPost, "/project/create", strings.NewReader(`{"name":"test1","userid":1}`))
+	req2.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec2 := httptest.NewRecorder()
+	c2 := e2.NewContext(req2, rec2)
+
+	recMap := map[string]*httptest.ResponseRecorder{"case1": rec1, "case2": rec2}
 
 	type fields struct {
 		ps service.ProjectServiceIF
@@ -39,15 +54,44 @@ func Test_projectController_CreateProject(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{"case1", fields{mock}, args{c1}, false},
+		{"case1", fields{mock1}, args{c1}, false},
+		{"case2", fields{mock2}, args{c2}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			pc := &projectController{
 				ps: tt.fields.ps,
 			}
+
 			if err := pc.CreateProject(tt.args.c); (err != nil) != tt.wantErr {
 				t.Errorf("projectController.CreateProject() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			rec := recMap[tt.name]
+			expectedHttpStatusCode := 0
+			switch tt.name {
+			case "case1":
+				expectedHttpStatusCode = http.StatusOK
+			case "case2":
+				expectedHttpStatusCode = http.StatusInternalServerError
+			}
+
+			if expectedHttpStatusCode != 0 && rec.Code != expectedHttpStatusCode {
+				t.Errorf("expected status %d; got %d", expectedHttpStatusCode, rec.Code)
+
+			}
+
+			tampJSON := `{"message":"%s"}`
+
+			expectedJSON := ""
+			switch tt.name {
+
+			case "case2":
+				expectedJSON = fmt.Sprintf(tampJSON, domain.ErrorMsg13)
+			}
+
+			if expectedJSON != "" {
+				assert.JSONEq(t, expectedJSON, rec.Body.String(), fmt.Sprintf("expected JSON: %s; actual JSON: %s", expectedJSON, rec.Body.String()))
 			}
 		})
 	}
