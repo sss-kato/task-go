182b1839c37ccc22783d758dfdff1c3ae55bc7b0
sss-kato <syunpei.1124@gmail.com>
2024-10-10 21:38:21 +0900

    プロジェクトドメイン　テストコード修正

--- domain/project_test.go
+++ domain/project_test.go
@@ -1,59 +1,73 @@
-// package domain
+package domain
 
-// import (
-// 	"reflect"
-// 	"testing"
-// )
+import (
+	"reflect"
+	"testing"
+)
 
-// func TestNewProject(t *testing.T) {
+func TestNewProject(t *testing.T) {
 
-// 	test_project_1 := makeTestProject("project_test_1", 1)
+	test_project_1 := makeTestProject("project_test_1", 1)
 
-// 	type args struct {
-// 		pnm string
-// 		uid int
-// 	}
-// 	tests := []struct {
-// 		name    string
-// 		args    args
-// 		want    ProjectIF
-// 		wantErr bool
-// 	}{
-// 		// TODO: Add test cases.
-// 		{"case1", args{pnm: test_project_1.GetProjectName(), uid: test_project_1.GetUserID()}, test_project_1, false},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got, err := NewProject(tt.args.pnm, tt.args.uid)
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("NewProject() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-// 			if !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("NewProject() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+	type args struct {
+		pnm string
+		uid int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    ProjectIF
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"case1", args{pnm: test_project_1.GetProjectName(), uid: test_project_1.GetUserID()}, test_project_1, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewProject(tt.args.pnm, tt.args.uid)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewProject() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 
-// type test_project struct {
-// 	name string
-// 	uid  int
-// }
+			if tt.name == "case1" {
+				gottype := reflect.TypeOf(got)
+				if !gottype.Implements(reflect.TypeOf((*ProjectIF)(nil)).Elem()) {
+					t.Errorf("%v does not implement ProjectIF", got)
+					return
+				}
 
-// func makeTestProject(name string, uid int) ProjectIF {
+				if got.GetProjectName() != test_project_1.GetProjectName() {
+					t.Errorf("NewProject() got = %v, want %v", err, tt.wantErr)
+					return
+				}
 
-// 	return &test_project{name, uid}
-// }
+				if got.GetUserID() != test_project_1.GetUserID() {
+					t.Errorf("NewProject() got = %v, want %v", err, tt.wantErr)
+					return
+				}
 
-// func (tp *test_project) GetProjectName() string {
+			}
+		})
+	}
+}
 
-// 	return tp.name
-// }
+type test_project struct {
+	name string
+	uid  int
+}
 
-// func (tp *test_project) GetUserID() int {
+func makeTestProject(name string, uid int) ProjectIF {
 
-// 	return tp.uid
-// }
+	return &test_project{name, uid}
+}
 
+func (tp *test_project) GetProjectName() string {
 
+	return tp.name
+}
+
+func (tp *test_project) GetUserID() int {
+
+	return tp.uid
+}
